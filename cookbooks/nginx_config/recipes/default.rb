ec6369251d9ecab4d94e700e16b892af1cef12cc
Daniel Nelson <dnelson@bluejade.com>
2010-06-10 21:07:15 -0500

    don't delete files since Chef first makes a copy

--- cookbooks/nginx_config/recipes/default.rb
+++ cookbooks/nginx_config/recipes/default.rb
@@ -5,18 +5,18 @@
 
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
 
-  file '/data/nginx/servers/iwheel.rewrites' do
-    action :delete
-  end
+  # file '/data/nginx/servers/iwheel.rewrites' do
+  #   action :delete
+  # end
 
   link '/data/nginx/servers/iwheel.rewrites' do
     to '/data/nginx_customizations/keep.iwheel.rewrites'
   end
 
 
-  file '/data/nginx/servers/iwheel.conf' do
-    action :delete
-  end
+  # file '/data/nginx/servers/iwheel.conf' do
+  #   action :delete
+  # end
 
   link '/data/nginx/servers/iwheel.conf' do
     to '/data/nginx_customizations/keep.iwheel.conf'
